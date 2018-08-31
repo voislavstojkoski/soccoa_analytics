788fb1122c40eeb46b890fe4a3e5d678ed9206d1
Voislav Stojkoski <stojkoski.official@gmail.com>
2018-08-31 14:24:41 +0200

    Changed to framework instead of Application type project

---
+++ SoccoAnalytics/SoccoAnalytics/SoccoAnalytics.h
@@ -0,0 +1,20 @@
+//
+//  SoccoAnalytics.h
+//  SoccoAnalytics
+//
+//  Created by massiveadmin on 8/31/18.
+//  Copyright © 2018 massiveadmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <FirebaseCore/FirebaseCore.h>
+
+//! Project version number for SoccoAnalytics.
+FOUNDATION_EXPORT double SoccoAnalyticsVersionNumber;
+
+//! Project version string for SoccoAnalytics.
+FOUNDATION_EXPORT const unsigned char SoccoAnalyticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SoccoAnalytics/PublicHeader.h>
+
+
